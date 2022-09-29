0c8b8de201937931cee782d4ec43ea32d64d7bf0
Scott Houser <Scottanthonyhouser@gmail.com>
2022-09-29 00:50:48 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
+import React, { useState, useMemo } from 'react';
+import { testData } from './TestData/testTransactions'
 import './App.css';
 
 function App() {
+  const [isLoading, setIsLoading] = useState('')
+  const [customerData, setCustomerData] = useState([])
+  const memoizedResults = useMemo(() => returnCustomerMap(customerData), [customerData]);
+
+  const randomIntFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+  
+ const fakeApi = () => {
+    let timeout = randomIntFromInterval(1000, 2000)
+    console.log(timeout)
+    setIsLoading(true)
+    return new Promise(resolve => {
+      setTimeout(() => resolve(testData), timeout);
+    });
+  };
+
+  const callApi = async () => {
+    const newText = await fakeApi();
+    setCustomerData(newText)
+    setIsLoading(false)
+  };
+
+  const returnLoadingIndicator = () => {
+    if(isLoading){
+      return 'Loading...'
+    }else{
+      return ''
+    }
+  }
+
+  const returnLoadButton = () => {
+    if(!isLoading && customerData.length < 1){
+      return(
+        <button
+          className='app-button'
+          onClick={()=>{callApi()}}
+        >
+          {'Load Customers'}
+        </button>
+      )
+    }
+  }
+
+  const makeCustomerTable = (obj) => {
+    if(obj){
+      return(
+        Object.entries(obj).map((item) => {
+          return(
+            <div className='table-row' key={item[0]}>
+              <div className='flex'>{`${item[1].customerName} : `}</div>
+              <div className='flex points-total'>{Math.floor(item[1].pointsForPurchace)}</div>
+            </div>
+          )
+        })
+      )
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {returnLoadButton()}
+        {returnLoadingIndicator()}
+        {makeCustomerTable(memoizedResults)}
       </header>
     </div>
   );
+}
+
+const returnCustomerMap = (array) => {
+
+  const calculatePointsForPurchace = (total) => {
+    const roundedTotal = Math.floor(total)
+    const totalLessThan50 = roundedTotal < 50
+    const totalMoreThan100 = roundedTotal > 100
+
+    if(totalLessThan50){
+      return 0
+    }else if(totalMoreThan100){
+      return 50 + ((total - 100) * 2)
+    }else{
+      return total - 50
+    }
+  }
+
+  let returnMap = {}
+
+  if(array.length > 0) {
+    array.forEach(object => {
+      let pointsForPurchace = calculatePointsForPurchace(object.purchaceTotal)
+      if(!returnMap.hasOwnProperty(object.customerId)){
+        returnMap[object.customerId] = {pointsForPurchace: pointsForPurchace, customerName: object.customerName};
+      }else{
+        let prevTotal = Math.floor(returnMap[object.customerId].pointsForPurchace)
+        returnMap[object.customerId].pointsForPurchace =  Math.floor(prevTotal + pointsForPurchace)
+      }
+    })
+  }
+
+  return returnMap
 }
 
 export default App;
---
+++ src/TestData/testTransactions.js
@@ -0,0 +1,93 @@
+export const testData = [ //contains two customers
+    {
+      customerName:'Scott Houser',
+      transactionId:'9ba0ee1d-e0d1-4c62-b0e3-111604ddf2e0',
+      customerId:'21917390-2fbb-46b0-abf1-019912c9dd38',
+      transactionTime: 1664388519,
+      purchaceTotal: 276.09
+    },
+      {
+        customerName:'Scott Houser',
+        transactionId:'11201363-4976-433a-af44-e3e6856d8b09',
+        customerId:'21917390-2fbb-46b0-abf1-019912c9dd38',
+        transactionTime: 1664273153,
+        purchaceTotal: 198.09
+      },
+      {
+        customerName:'Scott Houser',
+        transactionId:'ec455a07-6a5a-4b88-a860-5c7213e161f3',
+        customerId:'21917390-2fbb-46b0-abf1-019912c9dd38',
+        transactionTime: 1664365041,
+        purchaceTotal: 15.89
+      },
+      {
+        customerName:'Scott Houser',
+        transactionId:'6e07a151-cfd8-4636-b99b-099dac3b33fa',
+        customerId:'21917390-2fbb-46b0-abf1-019912c9dd38',
+        transactionTime: 1664278641,
+        purchaceTotal: 65.09
+      },
+      {
+        customerName:'Paul Houser',
+        transactionId:'de842b29-a136-41ed-9b11-2965b3040991',
+        customerId:'f47ece4e-cd7f-469a-8ab9-d9d9a2925ef0',
+        transactionTime: 1661594753,
+        purchaceTotal: 10.09
+      },
+      {
+        customerName:'Paul Houser',
+        transactionId:'6101e89f-0077-4ead-b715-ef72dd477864',
+        customerId:'f47ece4e-cd7f-469a-8ab9-d9d9a2925ef0',
+        transactionTime: 1664192241,
+        purchaceTotal: 60.09
+      },
+      {
+        customerName:'Paul Houser',
+        transactionId:'830f7668-53c9-4eac-a9de-84ec431c5c20',
+        customerId:'f47ece4e-cd7f-469a-8ab9-d9d9a2925ef0',
+        transactionTime: 1664278641,
+        purchaceTotal: 120.09
+      },
+      {
+        customerName:'Paul Houser',
+        transactionId:'7ddfc160-0a57-4ab0-b6c6-aaa2b6c7597d',
+        customerId:'f47ece4e-cd7f-469a-8ab9-d9d9a2925ef0',
+        transactionTime: 1664019441,
+        purchaceTotal: 4.09
+      },
+      {
+        customerName:'Paul Houser',
+        transactionId:'d62fc548-4a17-453d-89c4-4498fa81197a',
+        customerId:'f47ece4e-cd7f-469a-8ab9-d9d9a2925ef0',
+        transactionTime: 1663933041,
+        purchaceTotal: 49.09
+      },
+      {
+        customerName:'Paul Houser',
+        transactionId:'91add9d1-7ae2-4f97-9d69-793a9c4e8bd3',
+        customerId:'f47ece4e-cd7f-469a-8ab9-d9d9a2925ef0',
+        transactionTime: 1663846641,
+        purchaceTotal: 78.09
+      },
+      {
+        customerName:'John FiftyOne',
+        transactionId:'d096214d-4e4f-4578-bd1a-dee78da87f5d',
+        customerId:'6081b4f4-73f9-4eee-9f98-b255a1fbea8b',
+        transactionTime: 1663846641,
+        purchaceTotal: 51.09
+      },
+      {
+        customerName:'Jack HundredOne',
+        transactionId:'e256190e-347a-4bd2-b230-da4f1105c472',
+        customerId:'c769e78b-bc44-4d88-8e60-b3f862f35074',
+        transactionTime: 1663846641,
+        purchaceTotal: 101.09
+      },
+      {
+        customerName:'Jim OneDollar',
+        transactionId:'b7204bd7-cda3-4a04-a5eb-1cda825e7dc9',
+        customerId:'d145597d-d395-47c2-ab81-1b3139d21021',
+        transactionTime: 1663846641,
+        purchaceTotal: 1.09
+      },
+]
